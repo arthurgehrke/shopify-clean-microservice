979126922fc2d91950945b5a17efde181ce23f95
arthurgehrke <arthurgehrke07@gmail.com>
2020-12-20 18:31:21 -0300

    refactor: return ok response in a object on GetProductsController

--- src/presentation/controllers/products/get-products-controller.ts
+++ src/presentation/controllers/products/get-products-controller.ts
@@ -9,7 +9,7 @@ export class GetProductsController implements Controller {
     try {
       const products = await this.getProducts.get();
 
-      return products.length ? ok(products) : noContent();
+      return products.length ? ok({ products }) : noContent();
     } catch (error) {
       return serverError(error);
     }
