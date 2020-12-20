fdf59632d8e93135d3c73326a1d333d0cbd1282f
arthurgehrke <arthurgehrke07@gmail.com>
2020-12-20 17:58:13 -0300

    test: add getProductsSpy to improve GetProductsController tests

--- src/presentation/controllers/products/get-products-controller.spec.ts
+++ src/presentation/controllers/products/get-products-controller.spec.ts
@@ -1,21 +1,37 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { noContent } from '@/presentation/helpers/http/http-helper';
 import { GetProductsController } from '@/presentation/controllers/products/get-products-controller';
+import { mockGetProducts } from '@/presentation/tests/mocks/get-products';
+import { GetProducts } from '@/domain/usecases/get-products';
+import { ProductModel } from '@/domain/models/product';
+
+class GetProductsSpy<R = any> implements GetProducts {
+  products: ProductModel[] = mockGetProducts();
+
+  async get(): Promise<ProductModel[]> {
+    return this.products;
+  }
+}
 
 type SutTypes = {
   sut: GetProductsController;
+  getProductsSpy: GetProductsSpy<ProductModel[]>;
 };
 
 const makeSut = (): SutTypes => {
-  const sut = new GetProductsController();
+  const getProductsSpy = new GetProductsSpy<ProductModel[]>();
+  const sut = new GetProductsController(getProductsSpy);
 
   return {
-    sut
+    sut,
+    getProductsSpy
   };
 };
 
 describe('GetProducts Controller', () => {
   test('Should return 204 if no product is returned', async () => {
-    const { sut } = makeSut();
+    const { sut, getProductsSpy } = makeSut();
+    getProductsSpy.products = [];
     const httpResponse = await sut.handle();
 
     expect(httpResponse).toEqual(noContent());
