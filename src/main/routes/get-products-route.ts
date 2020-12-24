796dd771b770b2f064e16f0cc3242714f58a229b
arthurgehrke <arthurgehrke07@gmail.com>
2020-12-24 16:45:05 -0300

    feat: add routes config

--- src/main/routes/get-products-route.ts
+++ src/main/routes/get-products-route.ts
@@ -4,5 +4,5 @@ import { adaptRoute } from '@/main/adapters/express-route-adapter';
 import { makeGetProductsController } from '@/main/factories/controllers/get-products';
 
 export default (router: Router): void => {
-  router.post('/get-products', adaptRoute(makeGetProductsController()));
+  router.get('/get-products', adaptRoute(makeGetProductsController()));
 };
