b4a55c6d36769310481df4e255e8ae3a04ba50ae
arthurgehrke <arthurgehrke07@gmail.com>
2020-12-24 17:48:57 -0300

    feat: add dotenv

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,3 +1,10 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 export default {
-  port: process.env.PORT || 8081
+  port: process.env.PORT || 8081,
+  shopify_api_key: process.env.SHOPIFY_API_KEY,
+  shopify_api_password: process.env.SHOPIFY_API_PASSWORD,
+  shopify_store_name: process.env.SHOPIFY_STORE_NAME
 };
